11a3ff155a01018f8d8e7e5d787998b48b01508a
Abhishek1380 <undeabhishek707@gmail.com>
2025-01-05 00:07:24 +0530

    Unnecessary files deleted

--- frontend/myapp/src/page/HomePage.js
+++ frontend/myapp/src/page/HomePage.js
@@ -10,13 +10,13 @@ import Blogs from '../components/Blogs/Blogs';
 import BlogsDetail from '../components/Blogs/BlogsDetail';
 import TreatmentDetails from '../components/Treatments/TreatmentDetails';
 import Review from '../components/Review/Review';
-// import Navbar1 from '../components/Navbar1/Navbar1';
-// import './HomePage.css';
+
+
 import Videos from '../components/Videos/Videos';
 import BookAppointment from '../components/BookAppointment/BookAppointment';
 import Services from '../components/Services/Services';
 import Why from '../components/Why/Why';
-import HomeAboutMe from '../components/HomeAboutMe/HomeAboutMe';
+
 
 
 
@@ -29,7 +29,7 @@ const HomePage = () => {
             <QuickDoctorInfo />
             <Services />
             <Why />
-            {/* <HomeAboutMe /> */}
+
             <MapAndTimings />
             <div className="text-center" style={{ margin: "50px auto" }}>
                 <h3>Reviews</h3>
