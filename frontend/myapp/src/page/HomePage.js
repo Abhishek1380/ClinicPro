80d66c1fc0d719408f56c9071dc4108c2688804f
Abhishek1380 <undeabhishek707@gmail.com>
2025-11-21 14:06:34 +0530

    npm run build

--- frontend/myapp/src/page/HomePage.js
+++ frontend/myapp/src/page/HomePage.js
@@ -1,38 +1,18 @@
 import React from 'react';
-import Navbar from '../components/Navbar/Navbar';
-import AboutMe from '../components/AboutMe/AboutMe';
-import Footer from '../components/Footer/Footer';
-import FAQ from '../components/FAQ/FAQ';
-import QuickDoctorInfo from '../components/QuickDoctorInfo/QuickDoctorInfo';
-import MapAndTimings from '../components/MapAndTimings/MapAndTimings';
-import Treatments from '../components/Treatments/Treatments';
-import Blogs from '../components/Blogs/Blogs';
-import BlogsDetail from '../components/Blogs/BlogsDetail';
-import TreatmentDetails from '../components/Treatments/TreatmentDetails';
-import Review from '../components/Review/Review';
-
-
-import Videos from '../components/Videos/Videos';
-import BookAppointment from '../components/BookAppointment/BookAppointment';
+import LandingPage from '../components/LandingPage/LandingPage';
 import Services from '../components/Services/Services';
 import Why from '../components/Why/Why';
+import MapAndTimings from '../components/MapAndTimings/MapAndTimings';
+import Review from '../components/Review/Review';
+import Videos from '../components/Videos/Videos';
 import MockFAQ from '../components/MockFAQ/MockFAQ';
-// import SampleFAQ from '../components/SampleFAQ/sampleFAQ';
-import LandingPage from '../components/LandingPage/LandingPage';
-
-
-
 
 const HomePage = () => {
     return (
         <>
-
-
-
             <LandingPage />
             <Services />
             <Why />
-
             <MapAndTimings />
             <div className="text-center" style={{ margin: "50px auto" }}>
                 <h3>Reviews</h3>
@@ -40,19 +20,6 @@ const HomePage = () => {
             </div>
             <Videos />
             <MockFAQ />
-
-
-
-
-
-            {/* <BookAppointment /> */}
-
-
-
-
-
-
-
         </>
     )
 }
